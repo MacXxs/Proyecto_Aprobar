d97ff656e5a819f92ecfdd74e82c084095166f72
criis16 <cristian.ayala96@gmail.com>
2016-05-13 13:42:02 +0200

    Add files via upload

--- Cita.hh
+++ Cita.hh
@@ -1,31 +1,56 @@
+/** @file Cita.hh
+	@brief Especificació de la classe Cita
+*/
+
 #ifndef CITA_HH
 #define CITA_HH
 
-#include"Frase.hh"
+#include "Frase.hh"
 
 #ifndef NO_DIAGRAM
-#include<string>
+#include <string>
+#include <vector>
 #endif
 
 
+/** @class Cita
+	@brief Representa una cita el text
+*/
+
 class Cita{
     
+public:
+	
+	// Constructora
+	
+    Cita();
+	
+	//Destructora
+    
+    ~Cita();
+    
+	// Modificadora
+	
+    void crear_cita(string &referencia, char& x, char& y, vector<Frase>& frases, string& autor, vector<string>& Titol);
+	
+	
+	//Consultores
+	
+	void consultar_autor() const;
+	
+	void consultar_referencia() const;
+	
+	void consultar_titol() const;
+	
+	void consultar_numini() const;
+	
+	void consultar_numfin() const;
+    
 private:
-    istringstream autor;
+    string autor;
     vector<string> Titol;
     int numini; //numero frase inicial
     int numfin; //numero frase final
     vector<Frase> vec_frase; //vector de Frases que formen la cita ordenat
     string referencia; //referencia de la cita
-	
-public:
-    Cita();
-    
-    ~Cita();
-    
-    void crear_cita(string &referencia, char& x, char& y, vector<Frase>& frases);
-    
-    
-    
-    
-	
+}
